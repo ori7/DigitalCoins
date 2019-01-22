378e71322e6d533a198ee07aa43e653c3de402bf
ori7 <orisuliman@gmail.com>
2019-01-23 01:01:55 +0200

    search option

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 $(document).ready(function () {
 
+    let allCoins = [];
+
     $("#searchButton").click(function (e) {
 
         e.preventDefault();
-        newArray = [allCoins].filter(function (coin) { return coin.match($("#searchInput").val) }); console.log(newArray);
+
+        const filterItems = (query) => {
+            return allCoins.filter(el => el.name.toLowerCase().indexOf(query.toLowerCase()) > -1);
+        };
+
+        const newArray = (filterItems($("#searchInput").val()));
+        $('#coin').empty();
         getList(newArray);
+
     });
 
     const coinTemplate = `
-        <div class="card" style="width: 18rem;">
+        <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">{{symbol}}</h5>
                     <p class="card-text">{{name}}</p>
-                    <button id="moreInfo" class="btn btn-primary">More Info</button>
+                    <button id="{{id}}" class="btn btn-primary" data-toggle="collapse" data-target="#{{id}}" aria-expanded="false" aria-controls="collapseExample">More Info</button>
                 </div>
         </div>
         `
@@ -41,8 +50,29 @@ $(document).ready(function () {
         let template = coinTemplate;
         template = template.replace("{{symbol}}", coin.symbol);
         template = template.replace("{{name}}", coin.name);
+        template = template.replace(/{{id}}/g, coin.id);
         $("#coin").append(template);
 
+    };
+
+    const collapseTemplate = `
+        <div class="collapse" id="bitcoin">
+            <div class="card card-body">
+                <img src="{{image}}">
+            </div>
+        </div>
+        `
+
+    function getCoin(coinData) {
+
+        let template = collapseTemplate;
+        template = template.replace("{{image}}", coinData.image.large);
+
+        const coin = $('#' + coinData.id);
+        (coin.parent().parent()).append(template);
+        console.log(coinData);
+        console.log(coinData.image.small);
+        console.log(coinData.market_data.current_price.usd);
     };
 
     function getData(callback, toGet) {
@@ -51,7 +81,10 @@ $(document).ready(function () {
             //let url = 'https://api.coingecko.com/api/v3/coins/list';
             var url = 'demo.json';
         }
-        else{};
+        else {
+            //var url = 'https://api.coingecko.com/api/v3/coins/' + toGet;
+            var url = 'demoB.json';
+        }
 
         $.ajax({
             url: url,
@@ -59,9 +92,18 @@ $(document).ready(function () {
         }).done(function (d) {
             if (typeof d === 'string')
                 d = JSON.parse(d);
+            allCoins = d;
             callback(d);
+            $('#coin>.card>.card-body>button').click(function (e) {
+                e.preventDefault();
+                getData(function (d) {
+                    getCoin(d);
+                }, this.id);
+            });
         })
     };
+
+
 
     $('#navbarSupportedContent>ul>li>a').click(function (e) {
         e.preventDefault();

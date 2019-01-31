d7ad21123db9ce0b6543c3009080377106ab3c83
ori7 <orisuliman@gmail.com>
2019-01-31 12:34:22 +0200

    Organization and documentation

--- main.js
+++ main.js
@@ -1,5 +1,54 @@
 $(document).ready(function () {
 
+    var arrayObjectsData = [];
+
+    function getData(callback, toGet) {
+
+        if (toGet === 'allCoins') {
+            //var url = 'https://api.coingecko.com/api/v3/coins/list';
+            var url = 'demo.json';
+        }
+        else if (Array.isArray(toGet)) {
+            let coins = (toGet.join()).toUpperCase();  //   Get all the coins from the array and convert them into one string in capital letters. 
+            coins = coins.replace(/COIN-/g, '');    //   Delete from the array prefix that not needed..
+            var url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + coins + '&tsyms=USD';
+        }
+        else {     //    The variable that passed is 'id'
+            var url = 'https://api.coingecko.com/api/v3/coins/' + toGet;
+            //var url = 'demoB.json';
+        };
+
+        $.ajax({
+            url: url,
+            method: 'GET'
+        }).done(function (d) {
+            if (typeof d === 'string')
+                d = JSON.parse(d);
+            callback(d);
+            if (toGet === 'allCoins') {
+                const index = arrayObjectsData.findIndex(x => x.coinId === 'allCoins');
+                if (index != -1) {
+                    arrayObjectsData[index].content = d;
+                    arrayObjectsData[index].lastClick = new Date().getTime();
+                }
+                else
+                    arrayObjectsData.push({ lastClick: new Date().getTime(), coinId: 'allCoins', content: d });
+            };
+        });
+    };
+
+    function getWithAjax(toGet) {
+
+        getData(function (d) {
+            if (toGet === 'allCoins')
+                getList(d);
+            else if (Array.isArray(toGet))
+                getReports(d);
+            else
+                getCollapse(d);
+        }, toGet);
+    };
+
     getWithAjax('allCoins');     //   Get all coins when the page loaded
 
     $("#searchButton").click(function (e) {
@@ -70,7 +119,7 @@ $(document).ready(function () {
         }
         keepToggleButton();    //    Activates a function that maintains the status of the toggele buttons.
         coinClick();
-        switchButton();
+        switchButton();   //    After 'keepToggleButton' function, need to activate this function, to listen to clicks on the switch Button.
         $("#searchForm").prop('hidden', false);    //   Show the search option in the list.
     };
 
@@ -86,43 +135,6 @@ $(document).ready(function () {
             </div>
         </div>
     `;
-
-    function getData(callback, toGet) {
-
-        if (toGet === 'allCoins') {
-            //var url = 'https://api.coingecko.com/api/v3/coins/list';
-            var url = 'demo.json';
-        }
-        else if (Array.isArray(toGet)) {
-            let coins = (toGet.join()).toUpperCase();  //   Get all the coins from the array and convert them into one string in capital letters. 
-            coins = coins.replace(/COIN-/g, '');    //   Delete from the array prefix that not needed..
-            var url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=' + coins + '&tsyms=USD';
-        }
-        else {     //    The variable that passed is 'id'
-            var url = 'https://api.coingecko.com/api/v3/coins/' + toGet;
-            //var url = 'demoB.json';
-        };
-
-        $.ajax({
-            url: url,
-            method: 'GET'
-        }).done(function (d) {
-            if (typeof d === 'string')
-                d = JSON.parse(d);
-            callback(d);
-            if (toGet === 'allCoins') {
-                const index = arrayObjectsData.findIndex(x => x.coinId === 'allCoins');
-                if (index != -1) {
-                    arrayObjectsData[index].content = d;
-                    arrayObjectsData[index].lastClick = new Date().getTime();
-                }
-                else
-                    arrayObjectsData.push({ lastClick: new Date().getTime(), coinId: 'allCoins', content: d });
-            };
-        });
-    };
-
-    var arrayObjectsData = [];
 
     function getCollapse(coinData) {
 
@@ -152,33 +164,21 @@ $(document).ready(function () {
                     arrayObjectsData.push({
                         lastClick: lastClick,
                         coinId: id
-                    });console.log('in');
+                    });
                     getWithAjax(id);
                 }
                 else {
                     cleanCoin(id);
                     const result = checkTime(id, 10);    //   Checks whether to get the content from the server or from the cache, according to the time passed from the last call (time is sent as a parameter in seconds)
-                    getContent(result, id, index);
+                    getContent(result, id);
                 };
             };
         });
     };
 
-    function getWithAjax(toGet) {
-
-        getData(function (d) {
-            if (toGet === 'allCoins')
-                getList(d);
-            else if (Array.isArray(toGet))
-                getReports(d);
-            else
-                getCollapse(d);
-        }, toGet);
-    };
-
     function buttonName(id) {
 
-        var isExpanded = $('#' + id).attr("aria-expanded");
+        const isExpanded = $('#' + id).attr("aria-expanded");
         switch (isExpanded) {
             case 'false':
                 $('#' + id).text('Close');
@@ -190,111 +190,11 @@ $(document).ready(function () {
         return isExpanded;
     };
 
-    var switchArray = [];
+    function cleanCoin(coinToClean) {
 
-    function switchButton() {
-
-        $('#coin>.card>.card-body .switch>.default').change(function (e) {
-            e.preventDefault();
-            if (this.checked) {
-                if (switchArray.length < 5)
-                    switchArray.push(this.id);
-                else
-                    openWindow(this.id);
-            }
-            else
-                switchArray.splice($.inArray(this.id, switchArray), 1);
-        });
+        const coin = $('#collapse-content-' + coinToClean);
+        coin.empty();   //    Delite the old data before get the new data
     };
-
-    function keepToggleButton() {
-        if (switchArray) {
-            for (let i = 0; i < switchArray.length; i++) {
-                $('#' + (switchArray[i].toLowerCase())).click();
-            };
-        };
-    };
-
-    const templateWindow = `
-        <form class="container" id="form">
-            <p>You have too much coins reports, remove one!</p>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="report" id="report1" value="{{coin1}}">
-                <label class="form-check-label" for="report1">Remove {{coin1}}</label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="report" id="report2" value="{{coin2}}">
-                <label class="form-check-label" for="report2">Remove {{coin2}}</label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="report" id="report3" value="{{coin3}}">
-                <label class="form-check-label" for="report3">Remove {{coin3}}</label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="report" id="report4" value="{{coin4}}">
-                <label class="form-check-label" for="report4">Remove {{coin4}}</label>
-            </div>
-            <div class="form-check">
-                <input class="form-check-input" type="radio" name="report" id="report5" value="{{coin5}}">
-                <label class="form-check-label" for="report5">Remove {{coin5}}</label>
-            </div>
-            <button onclick="window.close()" id="removeButton">Remove</button>
-            <button onclick="window.close()">Cancle</button>
-        </form>
-        `;
-
-    function openWindow(coinToAdd) {
-
-        let template = templateWindow;
-        template = template.replace(/{{coin1}}/g, switchArray[0]);
-        template = template.replace(/{{coin2}}/g, switchArray[1]);
-        template = template.replace(/{{coin3}}/g, switchArray[2]);
-        template = template.replace(/{{coin4}}/g, switchArray[3]);
-        template = template.replace(/{{coin5}}/g, switchArray[4]);
-
-        const myWindow = window.open("", "MsgWindow", "width=400, height=300, top=300, left=300");
-        myWindow.document.write(template);
-        const $w = $(myWindow.document.body);
-        $w.find("#removeButton").click(function (e) {
-            e.preventDefault();
-            const coinRemove = $w.find('input[name=report]:checked', '#Form').val();
-            switchArray.splice($.inArray(coinRemove, switchArray), 1);
-            switchArray.push(coinToAdd);
-            $('#' + coinRemove).click();
-            switchArray.push(coinToAdd);  //  The click delete the latest from the array so we need to add him another time! 
-        });
-    };
-
-    var interval;
-
-    $('#navbarSupportedContent>ul>li>a').click(function (e) {
-        e.preventDefault();
-        if (interval)
-            clearInterval(interval);
-        const href = $(this).attr('href');
-        $.ajax(`templates/${href}.html`).done(function (htmlContent) {
-            $('#main').html(htmlContent);
-            if (href === 'home') {
-                const result = checkTime('allCoins', 5);
-                //const index = arrayObjectsData.findIndex(object => object.coinId === 'allCoins');
-                getContent(result, 'allCoins');
-            }
-            else {
-                $("#searchForm").prop('hidden', 'hidden');   //   Hide search option from the page
-                if (href === 'reports') {
-                    if (switchArray.length == 0) {
-                        alert('There are no coins to display!');
-                        $("a[href='home']").click();
-                    }
-                    else {
-                        interval = setInterval(function () {
-                            getWithAjax(switchArray);
-                        }, 2000);
-                    };
-                };
-            };
-        });
-    });
 
     function checkTime(key, timeInSeconds) {
 
@@ -328,9 +228,114 @@ $(document).ready(function () {
         };
     };
 
-    function cleanCoin(coinToClean){
-        var coin = $('#collapse-content-' + coinToClean);
-        coin.empty();   //    Delite the old data before get the new data
+    var switchArray = [];
+
+    function switchButton() {
+
+        $('#coin>.card>.card-body .switch>.default').change(function (e) {
+            e.preventDefault();
+            if (this.checked) {
+                if (switchArray.length < 5)
+                    switchArray.push(this.id);
+                else
+                    openWindow(this.id);
+            }
+            else
+                switchArray.splice($.inArray(this.id, switchArray), 1);
+        });
     };
+
+    const templateWindow = `
+        <form class="container" id="form">
+            <p>You have too much coins reports, remove one!</p>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="report" id="report1" value="{{coin1}}">
+                <label class="form-check-label" for="report1">Remove {{coin1}}</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="report" id="report2" value="{{coin2}}">
+                <label class="form-check-label" for="report2">Remove {{coin2}}</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="report" id="report3" value="{{coin3}}">
+                <label class="form-check-label" for="report3">Remove {{coin3}}</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="report" id="report4" value="{{coin4}}">
+                <label class="form-check-label" for="report4">Remove {{coin4}}</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="report" id="report5" value="{{coin5}}">
+                <label class="form-check-label" for="report5">Remove {{coin5}}</label>
+            </div>
+            <button onclick="window.close()" id="removeCoinButton">Remove</button>
+            <button onclick="window.close()">Cancle</button>
+        </form>
+        `;
+
+    function openWindow(coinToAdd) {
+
+        let template = templateWindow;
+        template = template.replace(/{{coin1}}/g, switchArray[0]);
+        template = template.replace(/{{coin2}}/g, switchArray[1]);
+        template = template.replace(/{{coin3}}/g, switchArray[2]);
+        template = template.replace(/{{coin4}}/g, switchArray[3]);
+        template = template.replace(/{{coin5}}/g, switchArray[4]);
+
+        const myWindow = window.open("", "MsgWindow", "width=400, height=300, top=300, left=300");
+        myWindow.document.write(template);
+
+        const removeWindow = $(myWindow.document.body);
+        removeWindow.find("#removeCoinButton").click(function (e) {
+            e.preventDefault();
+            const coinToRemove = removeWindow.find('input[name=report]:checked', '#Form').val();
+            switchArray.splice($.inArray(coinToRemove, switchArray), 1);
+            switchArray.push(coinToAdd);
+            $('#' + coinToRemove).click();   //   To cancel the previous click on the screen.
+            switchArray.push(coinToAdd);  //  The click in the previous row delete the latest from the array, so we need to add him another time! 
+        });
+    };
+
+    function keepToggleButton() {
+        if (switchArray) {
+            for (let i = 0; i < switchArray.length; i++) {
+                $('#' + (switchArray[i].toLowerCase())).click();
+            };
+        };
+    };
+
+    var interval;
+
+    $('#navbarSupportedContent>ul>li>a').click(function (e) {
+        e.preventDefault();
+        if (interval)    //   If 'interval' exists, then switching to another page, should stop it.
+            clearInterval(interval);
+        const href = $(this).attr('href');
+        $.ajax(`templates/${href}.html`).done(function (htmlContent) {
+            $('#main').html(htmlContent);
+            if (href === 'home') {
+                const result = checkTime('allCoins', 5);   //   Checks whether to get the content from the server or from the cache, according to the time passed from the last call (time is sent as a parameter in seconds)
+                getContent(result, 'allCoins');
+            }
+            else {
+                $("#searchForm").prop('hidden', 'hidden');   //   Hide search option from the page
+                if (href === 'reports')
+                    reportPage();
+            };
+        });
+    });
+
+    function reportPage() {
+
+        if (switchArray.length == 0) {
+            alert('There are no coins to display!');
+            $("a[href='home']").click();    //   Return to home page
+        }
+        else {
+            interval = setInterval(function () {
+                getWithAjax(switchArray);
+            }, 2000);
+        };
+    }
 
 });
